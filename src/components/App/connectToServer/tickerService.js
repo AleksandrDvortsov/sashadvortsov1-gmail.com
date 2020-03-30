e4d5f7244ba549f213651d63d942eb2d55410695
Alex <sashadvortsov1@gmail.com>
2020-03-30 16:36:40 +0300

    add connectToServer and redid component App

---
+++ src/components/App/connectToServer/tickerService.js
@@ -0,0 +1,27 @@
+import io from 'socket.io-client';
+import { isServerCorrectRespons } from '../service';
+
+let socket = null;
+
+export const myConnect = (stockSymbol, setData) => {
+    socket = io('http://localhost:4000');
+
+    socket.on('connect', () => {
+        console.log('connected');
+
+        socket.on(stockSymbol, (data) => {
+            if (isServerCorrectRespons(JSON.parse(data))) setData(JSON.parse(data))
+        });
+
+        socket.emit('ticker', stockSymbol);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('disconnected');
+    });
+};
+
+export const timeChangerToServer = (time) => {
+    // set time to server 
+    console.log('request tot server ', time)
+} 
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import './style.scss';
+import { connect } from 'react-redux';
+import { myConnect } from './connectToServer';
+import { setDataAction } from './actions';
 
-function App() {
+import BlockServerTime from './components/BlockServerTime';
+import FormBlock from './components/FormBlock';
+import ServerError from './components/ServerError';
+
+function App(props) {
+    const {
+        data, setData
+    } = props;
+
+    useEffect(() => {
+        myConnect('AAPL', setData);
+    }, []);
 
     return (
-        <div>my App</div>
-    )
+        <div >
+            {data.ticker !== undefined ? <div id='myApp' >
+                <BlockServerTime />
+                <FormBlock data={data} />
+            </div> : <ServerError />}
+        </div>
+    );
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setData: (data) => {
+            dispatch(setDataAction(data));
+        },
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        data: state.info_data.data,
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
